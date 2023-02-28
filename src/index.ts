a78ab18219e2e4b074e341b44ed51812f3972086
riyanto <riyantonugroho76@gmail.com>
2023-02-28 17:56:13 +0700

    update index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+
+    res.send({
+        "hallo": 4
+    })
+})
+
 app.listen(3000, () => {
     console.log("server init 3000");
 
